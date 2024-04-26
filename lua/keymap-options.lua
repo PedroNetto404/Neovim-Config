490d2e9247fa1fb3ba5131b8508637ad09aec060
Super User <pedronetto31415@gmail.com>
2024-04-26 15:56:35 -0300

    finished setup

--- lua/keymap-options.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<C-Tab>', ':tabnext', {})
-vim.keymap.set('n', '<C-w>', ':close', {})
-vim.keymap.set('n', '<C-S-Tab>', ':tabprevious', {})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,14 @@
-return   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate", 
-
-  config = function() 
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { 
-        "lua", 
-        "javascript", 
-        "c",
-        "query",
-        "vim",
-        "html",
-        "c_sharp"
-      },
-      highlight = { 
-        enable = true
-      },
-      indent = {
-        enable = true
-      }
-    })
-  end
-} 
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  }
+}
